70ee98cbda72256933ffa93b364438e7c295e435
dereckdemezquita <dereck@demezquita.com>
2020-02-20 15:55:57 +0100

    Initial commit.

---
+++ R/save_dated_df.R
@@ -0,0 +1,31 @@
+#' A function for easily saving time stamped data frames.
+#'
+#' Takes one of two save functions, "excel" or "robject", data frame, and name
+#' @param save_function A file format either "excel" or "robject"; must be class of character
+#' @param df A data frame
+#' @param name A name for the file
+#' @export
+save_dated_df <- function(save_function, df, name) {
+	save_function_class <- class(save_function)
+	name_class <- class(name)
+	if(save_function != "character" && name_class != "character") {
+		stop("Please use character types for save_function and name inputs.")
+	}
+
+	generate_dated_name <- function(name) {
+		date_time <- Sys.time() %>% as.character() %>% str_replace(" ", "_") %>% str_replace_all(":", "-")
+		dated_name <- paste(date_time, name, sep = "_")
+
+		return(dated_name)
+	}
+
+	if(save_function == "excel") {
+		dated_excel_name <- generate_dated_name(name) %>% paste(".xlsx", sep = "")
+		write.xlsx(df, file = dated_excel_name, colNames = TRUE, rowNames = TRUE, borders = "surrounding")
+	} else if(save_function == "robject") {
+		dated_robject_name <- generate_dated_name(name) %>% paste(".RData", sep = "")
+		save(df, file = dated_robject_name)
+	} else {
+		message("Please select either \"excel\" or \"robject\" for a \"save_function\".")
+	}
+}
---
+++ README.md
@@ -0,0 +1,19 @@
+# Octavius
+## A network inferencing and machine learning tool-kit
+
+This package will be developed over time, and stored here, ready for download and install.
+
+To install the package please use devtools with the following commands:
+
+```
+library(devtools)
+install_github("dereckdemezquita/octavius")
+```
+
+## Package information
+* Author: Dereck de Mézquita
+* Affiliation: University Paris-Saclay
+* Department: GenHotel
+* Title: Computational biology; network inferencing and machine learning
+* Contact: <dereck@demezquita.com>
+* Website: <https://www.derecksnotes.com>

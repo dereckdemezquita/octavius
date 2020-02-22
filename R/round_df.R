d572cace496c61151b54964d228cd81a0ca6eaee
dereckdemezquita <dereck@demezquita.com>
2020-02-22 15:45:00 +0100

    Added a round data frame rounding function.

---
+++ R/round_df.R
@@ -0,0 +1,14 @@
+#' Function for rounding all values in a data frame keeping them in the numeric type.
+#'
+#' Takes two variables, a data frame and a number to round to.
+#' @param df a data frame input.
+#' @param digits a number to which the values will be rounded to.
+#' @export
+round_df <- function(df, digits) {
+	# round all numeric variables
+	# df: data frame
+	# digits: number of digits to round enter numeric type
+	numeric_columns <- sapply(df, mode) == 'numeric'
+	df[numeric_columns] <-  round(df[numeric_columns], digits)
+	df
+}

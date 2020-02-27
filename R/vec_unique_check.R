6aa822718279fe510733e4f3987acb6c992a69b4
dereckdemezquita <dereck@demezquita.com>
2020-02-27 17:16:20 +0100

    Fixed the uniqueness vector check function.

--- R/vec_unique_check.R
+++ R/vec_unique_check.R
@@ -3,7 +3,10 @@
 #' A function that will take a vector and test if the values are unique.
 #' @param vec A vector of any kind, can contain logicals or characters etc.
 #' @export
-vec_unique_check <- function(vec) {
-	message("No repeats found in the vector input.")
-	# return(!any(duplicated(vec)))
+vec_unique_check <- function(vector) {
+	if(!any(duplicated(vector))) {
+		message("No repeat values found in the vector.")
+	} else {
+		message("Repeat values found in the vector.")
+	}
 }

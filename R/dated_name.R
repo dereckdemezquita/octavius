a3e016982bce3c4058aee3ff7da40da995f43d24
dereckdemezquita <dereck@demezquita.com>
2020-04-03 13:16:00 +0200

    Added `sep` option to dated functions.

--- R/dated_name.R
+++ R/dated_name.R
@@ -4,10 +4,10 @@
 #' Takes a character input
 #' @param name A name to concatenate as a suffix.
 #' @export
-dated_name <- function(name) {
+dated_name <- function(name, sep) {
 	date_time <- Sys.time() %>% as.character() %>% str_replace(" ", "_") %>% str_replace_all(":", "-")
 	date_time <- str_remove_all(date_time, "-") # Remove "-" from date names
-	dated_name <- paste(date_time, name, sep = "_")
+	dated_name <- glue::glue("{date_time}_{name}")
 
 	return(dated_name)
 }
--- R/dated_save.R
+++ R/dated_save.R
@@ -6,34 +6,28 @@
 #' @param input A data frame.
 #' @param name A name for the file.
 #' @export
-dated_save <- function(input, save_function, name) {
+dated_save <- function(input, save_function, name, sep) {
 	save_function_class <- class(save_function)
 	name_class <- class(name)
 	if(save_function != "character" && name_class != "character") {
 		stop("Please use character types for save_function and name inputs.")
 	}
 
-	generate_dated_name <- function(name) {
-		date_time <- Sys.time() %>% as.character() %>% str_replace(" ", "_") %>% str_replace_all(":", "-")
-		date_time <- str_remove_all(date_time, "-") # Remove "-" from date names
-		dated_name <- paste(date_time, name, sep = "_")
+	dated_name <- dated_name(name, sep)
 
-		return(dated_name)
-	}
-
-	dated_name <- generate_dated_name(name)
+	print(glue::glue("You saved {dated_name}"))
 
 	if(save_function == "txt") {
-		dated_txt_name <- dated_name %>% paste(".txt", sep = "")
+		dated_txt_name <- dated_name %>% paste(".txt", sep = sep)
 		write.table(input, file = dated_txt_name, quote = F, row.names = F, col.names = F)
 	} else if(save_function == "csv") {
-		dated_csv_name <- dated_name %>% paste(".csv", sep = "")
+		dated_csv_name <- dated_name %>% paste(".csv", sep = sep)
 		write.csv(input, file = dated_csv_name)
 	} else if(save_function == "excel") {
-		dated_excel_name <- dated_name %>% paste(".xlsx", sep = "")
+		dated_excel_name <- dated_name %>% paste(".xlsx", sep = sep)
 		write.xlsx(input, file = dated_excel_name, colNames = TRUE, rowNames = TRUE)
 	} else if(save_function == "robject") {
-		dated_robject_name <- dated_name %>% paste(".RData", sep = "")
+		dated_robject_name <- dated_name %>% paste(".RData", sep = sep)
 		save(input, file = dated_robject_name)
 	} else {
 		message("Please select either \"txt\", \"csv\", \"excel\", or \"robject\" for a \"save_function\".")

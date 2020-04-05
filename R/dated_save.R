74b568a0e23e580a77a092b089e302cb9cf8d32b
dereckdemezquita <dereck@demezquita.com>
2020-04-05 23:37:33 +0200

    Corrected separator error in dated_save().

--- R/dated_save.R
+++ R/dated_save.R
@@ -6,28 +6,28 @@
 #' @param input A data frame.
 #' @param name A name for the file.
 #' @export
-dated_save <- function(input, save_function, name, sep) {
+dated_save <- function(input, save_function, name) {
 	save_function_class <- class(save_function)
 	name_class <- class(name)
 	if(save_function != "character" && name_class != "character") {
 		stop("Please use character types for save_function and name inputs.")
 	}
 
-	dated_name <- dated_name(name, sep)
+	dated_name <- dated_name(name)
 
 	print(glue::glue("You saved {dated_name}"))
 
 	if(save_function == "txt") {
-		dated_txt_name <- dated_name %>% paste(".txt", sep = sep)
+		dated_txt_name <- dated_name %>% paste0(".txt")
 		write.table(input, file = dated_txt_name, quote = F, row.names = F, col.names = F)
 	} else if(save_function == "csv") {
-		dated_csv_name <- dated_name %>% paste(".csv", sep = sep)
+		dated_csv_name <- dated_name %>% paste0(".csv")
 		write.csv(input, file = dated_csv_name)
 	} else if(save_function == "excel") {
-		dated_excel_name <- dated_name %>% paste(".xlsx", sep = sep)
+		dated_excel_name <- dated_name %>% paste0(".xlsx")
 		write.xlsx(input, file = dated_excel_name, colNames = TRUE, rowNames = TRUE)
 	} else if(save_function == "robject") {
-		dated_robject_name <- dated_name %>% paste(".RData", sep = sep)
+		dated_robject_name <- dated_name %>% paste0(".RData")
 		save(input, file = dated_robject_name)
 	} else {
 		message("Please select either \"txt\", \"csv\", \"excel\", or \"robject\" for a \"save_function\".")

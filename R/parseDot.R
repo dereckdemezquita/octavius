1b138fb4233863ff6e36e2ebbbe7bf05dadecf74
dereckdemezquita <dereck@demezquita.com>
2020-04-10 12:42:57 +0200

    Fixed and completed parseDot() funciton.

--- R/parseDot.R
+++ R/parseDot.R
@@ -8,7 +8,7 @@
 #' @param file A file path as a string.
 #' @export
 parseDot <- function(file) { # Will output two vars globally: dot_data, dot_meta; both concatenated with the dim size of the recovered file
-	dot <- read.csv(file, header = FALSE, sep = "-")
+	dot <- read.csv(carn1, header = FALSE, sep = "-")
 	dim <- eval({dim(dot)[1]}) %>% as.character()
 
 	dot_data_nm <- paste("dot_data", dim, sep = "_")
@@ -20,16 +20,18 @@ parseDot <- function(file) { # Will output two vars globally: dot_data, dot_meta
 	dot_meta <- dot[which(dot$target == ""), ] %>% head(-1) %>% tail(-1)
 	dot_meta$target <- NULL
 
+
 	dot_data_tmp <- dfRmvStr(dot_data, str1 = ">", str2 = " \\[", str3 =  "\\]", rep2 = ", ")
+	dot_data_tmp <- dfRmvStr(dot_data_tmp, str1 = " penwidth=", str2 = " color=", str3 = " arrowhead=")
 
 	dot_data <- separate(dot_data_tmp, col = "target", into = c("target", "penwidth", "colour", "arrow"), sep = ",", remove = FALSE) # Final data output
-	dot_data <-dfRmvStr(dot_data, str1 = "penwidth=", str2 = "color=", str2 = "arrowhead=", rep1 = "", rep2 = "", rep3 = "")
 
 	assign(dot_data_nm, value = dot_data, pos = 1) # Assign in the outer envir
 
 	dot_meta_tmp <- dfRmvStr(dot_meta, str1 = " \\[", str2 = "\\];", rep1 = ", ", rep2 = "")
+	dot_data_tmp <- dfRmvStr(dot_meta_tmp, str1 = " style=", str2 = " color=", str3 = " fillcolor=") %>% dfRmvStr(str1 = " shape=")
 
-	dot_meta_all <- separate(dot_meta_tmp, col = "origin", into = c("gene", "style", "colour", "fill", "shape"), sep = ",", remove = TRUE)
+	dot_meta_all <- separate(dot_data_tmp, col = "origin", into = c("gene", "style", "colour", "fill", "shape"), sep = ",", remove = TRUE)
 
 	dot_meta_top <- dot_meta_all[rowSums(!is.na(dot_meta_all)) == 5,]
 	dot_meta_btm <- dot_meta_all[rowSums(!is.na(dot_meta_all)) != 5,]
@@ -38,7 +40,6 @@ parseDot <- function(file) { # Will output two vars globally: dot_data, dot_meta
 	names(dot_meta_btm)[names(dot_meta_btm) == "colour"] <- "fill"
 
 	dot_meta <- bind_rows(dot_meta_top[c("gene", "style", "colour", "fill", "shape")], dot_meta_btm[c("gene", "style", "fill")]) # Final meta data output
-	dot_meta <- dfRmvStr(dot_meta, str1 = "style=", str2 = "color=", str2 = "fillcolor=", rep1 = "", rep2 = "", rep3 = "") %>% dfRmvStr(str1 = "shape=", rp1 = "")
 
 	assign(dot_meta_nm, value = dot_meta, pos = 1) # Assign in the outer envir
 

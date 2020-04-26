a923f8e974a076b613237848dd8fdade320430d1
dereckdemezquita <dereck@demezquita.com>
2020-04-26 13:28:09 +0200

    Fixed bug in dated_name() function name; to datedName().

--- R/datedName.R
+++ R/datedName.R
@@ -4,7 +4,7 @@
 #' Takes a character input
 #' @param name A name to concatenate as a suffix.
 #' @export
-dated_name <- function(name, sep) {
+datedName <- function(name, sep) {
 	date_time <- Sys.time() %>% as.character() %>% str_replace(" ", "_") %>% str_replace_all(":", "-")
 	date_time <- str_remove_all(date_time, "-") # Remove "-" from date names
 	dated_name <- glue::glue("{date_time}_{name}")
